60f0e473f036ae5e556ca18e12409596f10ee03e
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-04-09 23:50:05 +0200

    feat: using classes to get common data between stored

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,17 +1,35 @@
 <script setup>
 const props = defineProps({
-  product: {
-    type: Object,
-    required: true,
-  },
+    product: {
+        type: Object,
+        required: true,
+    },
+    id: {
+        type: Number,
+        required: true
+    }
 })
+const { data: result, status, error } = await useAsyncData(
+    `product-${props.id}`,
+    async () => await useScraper(props.product),
+    {
+        lazy: true
+    }
+)
 
+const test = computed(() => result.value)
+const test2 = computed(() => status.value)
+const test3 = computed(() => error.value)
 </script>
 <template>
-    <div class="border rounded p-5 flex flex-col gap-3 w-full">
-        <img v-if="product.info.image" :src="product.info.image" class="w-full h-auto"/> 
-        <a class="text-3xl font-bold hover:text-blue-600 hover:underline decoration-solid" :href="product.store.website">{{product.store.name}}</a>
-        <h4 class="text-xl">{{product.game.name}} - {{product.game.expansion}} ({{product.game.lang}})</h4>
-        <p v-if="product.info.price" class="ml-auto mt-4 text-lg">💸 Price: <strong>{{product.info.price }}</strong></p>
+    <div class="border rounded p-5 flex flex-col gap-3 w-full h-full">
+        <template v-if="status == 'pending'">{{status}}</template>
+        <template v-if="status == 'success'">
+            <img :src="result.info.image" class="w-full h-auto"/> 
+            <a class="text-2xl font-bold hover:text-blue-600 hover:underline decoration-solid" :href="result.store.website">{{result.store.name}}</a>
+            <h4 class="text-xl">{{result.game.name}} - {{result.game.set}} ({{result.game.lang}})</h4>
+            <p class="ml-auto mt-4 text-lg">💸 Price: <strong>{{result.info.price }}</strong></p>
+        </template>
+        <template v-if="status == 'error'">ERRORE</template>
     </div>
 </template>
--- composables/productList.js
+++ composables/productList.js
@@ -1,53 +1,30 @@
+
+import { IlCovoDelNerd, IlNuovoMondoShop } from '@/entities/entities.js'
 export function useProductList(){
     return [
-        {
-            store: {
-                name: "Il Covo del Nerd",
-                website: "https://www.ilcovodelnerd.com/"
-            },
-            game: {
+        new IlCovoDelNerd(
+            'https://www.ilcovodelnerd.com/shop/prodotti-sigillati/riftbound-league-of-legends/riftbound-league-of-legends-box-set-one-origins-24-bustine-eng/',
+            {
                 name: "Riftbound",
-                expansion: "Set One",
+                set: "Set One",
                 lang: "EN"
             },
-            scraper: {
-                url: "https://www.ilcovodelnerd.com/shop/prodotti-sigillati/riftbound-league-of-legends/riftbound-league-of-legends-box-set-one-origins-24-bustine-eng/",
-                priceSelector: "p.product-page-price bdi",
-                imageSelector: ".wp-post-image"
-            }
-            
-        },
-        {
-            store: {
-                name: "Il Covo del Nerd",
-                website: "https://www.ilcovodelnerd.com/"
-            },
-            game: {
+        ).getScraper(),
+        new IlCovoDelNerd(
+            'https://www.ilcovodelnerd.com/shop/prodotti-sigillati/altri-giochi-di-carte/one-piece-tcg/one-piece-cg-op-11-game-box-24-bustine-eng/',
+            {
                 name: "One Piece",
-                expansion: "OP-11",
+                set: "OP-11",
                 lang: "EN"
             },
-            scraper: {
-                url: "https://www.ilcovodelnerd.com/shop/prodotti-sigillati/altri-giochi-di-carte/one-piece-tcg/one-piece-cg-op-11-game-box-24-bustine-eng/",
-                priceSelector: "p.product-page-price bdi",
-                imageSelector: ".wp-post-image"
-            }
-        },
-        {
-            store: {
-                name: "Il nuovo mondo shop",
-                website: "https://www.ilnuovomondoshop.it/"
-            },
-            game: {
+        ).getScraper(),
+        new IlNuovoMondoShop(
+            'https://www.ilnuovomondoshop.it/one-piece-tcg/one-piece-card-game-box-da-24-buste-op-09',
+            {
                 name: "One Piece",
-                expansion: "OP-09",
+                set: "OP-09",
                 lang: "EN"
             },
-            scraper: {
-                url: "https://www.ilnuovomondoshop.it/one-piece-tcg/one-piece-card-game-box-da-24-buste-op-09",
-                priceSelector: ".current-price-value",
-                imageSelector: ".js-qv-product-cover"
-            }
-        },
+        ).getScraper(),
     ]
 } 

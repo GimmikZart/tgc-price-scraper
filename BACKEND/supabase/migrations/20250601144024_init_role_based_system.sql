e44129e78071635c6c8ccbf9410f73bc03b669a2
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-06-01 16:41:36 +0200

    feat: migration for init role system

---
+++ BACKEND/supabase/migrations/20250601144024_init_role_based_system.sql
@@ -0,0 +1,72 @@
+create type "public"."profile_role" as enum ('user', 'admin', 'moderator');
+
+create table "public"."profiles" (
+    "id" uuid not null default auth.uid(),
+    "created_at" timestamp with time zone not null default now(),
+    "username" text,
+    "user_tag" text,
+    "role" profile_role not null default 'user'::profile_role,
+    "email" text
+);
+
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+  insert into public.profiles (id, email)
+  values (new.id, new.email);
+  return new;
+end;$function$
+;
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+

3834e5a12304b2e3b8b26e40e1fd95a7969a18c4
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-06-06 18:58:14 +0200

    refactor: cleaned code

--- FRONTEND/pages/user.vue
+++ FRONTEND/pages/user.vue
@@ -4,14 +4,6 @@
     <Toolbar label="User"> </Toolbar>
     <v-container class="flex flex-col justify-between grow relative gap-5">
       <div>
-        <!--  <div class="w-full h-[100px] rounded relative">
-          <img
-            class="absolute w-full h-full object-cover rounded"
-            src="https://en.onepiece-cardgame.com/images/cardlist/card/OP09-062.png"
-            alt=""
-          />
-          <v-btn height="100%" block color="transparent">Collezione</v-btn>
-        </div> -->
         <ButtonRouter
           to="/collection"
           disabled

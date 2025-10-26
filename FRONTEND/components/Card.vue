99fdc4449c0c00fee334218a4c875185582cff81
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-10-26 20:45:12 +0100

    refactor: streamline Card component structure and improve code readability

--- FRONTEND/components/Card.vue
+++ FRONTEND/components/Card.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { Icon } from "@iconify/vue";
+import { ref, computed } from "vue";
+
 const props = defineProps({
   card: { type: Object, required: true },
   handleCards: { type: Boolean, default: false },
@@ -10,57 +12,47 @@ const props = defineProps({
 const emit = defineEmits(["remove-card", "add-card", "choose-card", "open"]);
 const isLoaded = ref(false);
 
-const cardClass = computed(() => {
-  return { 
-    'border-[1px] border-white/30 rounded-lg': props.handleCards || !isLoaded.value ,
-    'relative': !isLoaded.value 
-  }
-})
+const cardClass = computed(() => ({
+  "border-[1px] border-white/30 rounded-lg": props.handleCards || !isLoaded.value,
+}));
 
-
-function onLoad() {
-  isLoaded.value = true;
-}
-function openCard() {
-  if (!props.disableOpening) emit("open", props.card);
-}
-
+function onLoad() { isLoaded.value = true; }
+function openCard() { if (!props.disableOpening) emit("open", props.card); }
 </script>
 
 <template>
-  <div :key="card.id" class="h-auto flex flex-col justify-between overflow-hidden" :class="cardClass">
-    <h5 v-if="!isLoaded" class="absolute bottom-0 left-0 w-full text-italic text-center py-3 truncate text-sm z-[2]">{{ card.name }}</h5>
-    <v-skeleton-loader type="image" v-if="!isLoaded" color="black" class="image-skeleton w-full overflow-hidden aspect-[63/88]" />
+  <div :key="card.id" class="h-auto flex flex-col overflow-hidden" :class="cardClass">
+    <!-- Skeleton -->
+    <v-skeleton-loader
+      v-if="!isLoaded"
+      type="image"
+      color="black"
+      class="image-skeleton w-full overflow-hidden aspect-[63/88]"
+    />
+
+    <!-- Immagine -->
     <NuxtImg
       v-show="card.image"
       :src="card.image"
       format="webp"
       loading="lazy"
-      width="100%"
-      class="border shadow-md cursor-zoom-in z-2"
-      :class="{
-        'h-[1px]': !isLoaded,
-        'h-auto': isLoaded
-      }"
+      class="border shadow-md cursor-zoom-in block w-full"
+      :class="{ 'h-[1px]': !isLoaded, 'h-auto': isLoaded }"
       fit="cover"
       :alt="card.name"
       @load="onLoad"
-      @click.stop="openCard()"
+      @click="openCard()"
+      placeholder
     />
 
-    <!-- Tasti collezione -->
-    <Transition
-      appear
-      enter-active-class="transition-all duration-300 ease-out"
-      enter-from-class="-translate-y-full"
-      enter-to-class="translate-y-0"
-      leave-active-class="transition-all duration-0 ease-out"
-      leave-from-class="translate-y-0"
-      leave-to-class="-translate-y-full"
-    >
-      <div v-if="handleCards && isLoaded" class="flex gap-3 items-center justify-between -z-1">
-        <div class="w-full flex items-center justify-between">
-          <v-btn variant="tonal" color="white" @click="$emit('remove-card')">
+    <!-- Tasti collezione - espansione in altezza -->
+    <v-expand-transition>
+      <div
+        v-if="handleCards && isLoaded"
+        class="overflow-hidden" 
+      >
+        <div class="flex gap-3 items-center justify-between px-1 py-1">
+          <v-btn variant="tonal" color="white" @click="cardCount >= 1 ? $emit('remove-card') : null">
             <v-icon size="25" color="red">mdi-minus</v-icon>
           </v-btn>
           <span class="text-xl">{{ cardCount }}</span>
@@ -69,7 +61,7 @@ function openCard() {
           </v-btn>
         </div>
       </div>
-    </Transition>
+    </v-expand-transition>
 
     <v-btn
       v-if="chooseCard"
@@ -82,9 +74,10 @@ function openCard() {
     </v-btn>
   </div>
 </template>
+
 <style>
 .v-skeleton-loader.image-skeleton .v-skeleton-loader__bone.v-skeleton-loader__image {
-    height: 100%;
-    border-radius: 0;
+  height: 100%;
+  border-radius: 0;
 }
 </style>

40cdb41e5caea40a820a550ddfd0738298c70210
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-11-12 18:40:15 +0100

    tasto prezzo rifattorizzato

--- FRONTEND/components/Card.vue
+++ FRONTEND/components/Card.vue
@@ -55,10 +55,9 @@ function openCard() { if (!props.disableOpening) emit("open", props.card); }
       />
       <div v-if="showPrice && isLoaded" class="p-1">
         <p class="text-xs truncate p-1">{{ card.setName }}</p>
-        <a class="w-full rounded-lg bg-black/70 flex justify-between text-black items-center px-4 py-1 block text-white text-center" :href="card.price ? card.slugs[0].url : '#'" target="_blank" rel="noopener noreferrer">
+        <a class="w-full rounded-lg bg-black/70 flex flex-col justify-between gap-1 text-black items-center px-4 py-1 block text-white text-center" :href="card.price ? card.slugs[0].url : '#'" target="_blank" rel="noopener noreferrer">
           <span class="text-xs">CardTrader</span>
-          <Icon class="text-xs" icon="mdi:arrow-right" />
-          <span class="font-bold text-xs">{{ card.price ?? '---' }} €</span>
+          <div class="w-full text-center font-bold text-xs">{{ card.price ?? '---' }} €</div>
         </a>
       </div>
       
